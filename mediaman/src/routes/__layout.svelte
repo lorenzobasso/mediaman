0bcaefc8b19e0f69e38819c3d9631c890eaa6d8d
lorenzobasso <lorenzobasso98@gmail.com>
2022-07-02 23:32:19 +0100

    Initial svelte app

---
+++ mediaman/src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<header>
+	<h1>Mediaman</h1>
+	<nav>cya</nav>
+</header>
+
+<main><slot /></main>
+
+<style>
+	header {
+		background-color: red;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 1rem;
+	}
+
+	h1 {
+		margin: 0;
+	}
+</style>
